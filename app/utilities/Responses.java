f2b13e4b5b6b53f246512e1c13610853903d64de
Valentin Rutz <valentin.rutz@epfl.ch>
2015-05-10 22:41:25 +0200

    Correct randint bug, add words to blacklist and correct font color problem

--- app/utilities/Responses.java
+++ app/utilities/Responses.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class Responses {
     private List<Response> responses;
-    private int pointer = new Random().nextInt();
+    private int pointer = Math.abs(new Random().nextInt());
     private LinkedList<Response> priorityResponses;
 
     public Responses() {
